49f658df64010f8a729cdf91888686f330fcef42
Abu Sayed <mr.sayedf@yandex.com>
2023-03-20 20:34:27 +0600

    portfolio add

---
+++ abusayed/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  alert("kdkd")
+  return (
+    <div className="App">
+      fgg
+    </div>
+  );
+}
+
+export default App;
